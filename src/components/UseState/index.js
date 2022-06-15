7bbfe69fedd9481ad98456485d021397e215733d
john0123456789 <johnluongpham@yahoo.com>
2022-06-15 09:10:15 -0700

    initial commit

--- src/components/UseState/index.js
+++ src/components/UseState/index.js
@@ -1,7 +1,27 @@
+import "./UseState.css";
+import { useState } from "react";
+
 const UseState = () => {
+  const [theme, setTheme] = useState("light");
+  const [count, setCount] = useState(0);
+
   return (
-    <div>
+    <div className={`state ${theme}`}>
       <h1>UseState Component</h1>
+      <button
+        onClick={() =>
+          theme === "dark" ? setTheme("light") : setTheme("dark")
+        }
+      >
+        Toggle Theme
+      </button>
+      <h1>{count}</h1>
+      <button onClick={() => setCount((prevCount) => prevCount + 1)}>
+        Increment
+      </button>
+      <button onClick={() => setCount((prevCount) => prevCount - 1)}>
+        Decrement
+      </button>
     </div>
   );
 };
